8b2721d9432defbc71c7bce444c6f67d22f85f87
rishirajbansal <rishiraj.architect@gmail.com>
2020-01-06 19:30:15 +0530

    Update docker-handler.dev-aws.sh

--- Deployment/Translator/ci-cd/jenkins/dev-aws/docker-handler.dev-aws.sh
+++ Deployment/Translator/ci-cd/jenkins/dev-aws/docker-handler.dev-aws.sh
@@ -18,7 +18,7 @@ then
 
     ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ec2-user@${AWS_NAT_INSTANCE_DNS} \
         PRIVATE_KEY_PATH=$PRIVATE_KEY_PATH AWS_DB_INSTANCE_DNS=$AWS_DB_INSTANCE_DNS CONTAINER_NAME=$DB_CONTAINER_NAME \
-        <<-'ENDSSH'
+        'sh -s' <<-'ENDSSH'
 echo "Teeeeeeeeeeeeeeeeeee"      
 ssh ubuntu@${AWS_DB_INSTANCE_DNS} CONTAINER_NAME=$CONTAINER_NAME 'bash -s' < aws-handler.sh stop-containers 
 exit
