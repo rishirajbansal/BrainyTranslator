97e1281f6ce14a64dcb52131604b4dbfb88f7827
rishirajbansal <rishiraj.architect@gmail.com>
2020-01-07 08:30:46 +0530

    Update aws-handler.sh

--- Deployment/Translator/ci-cd/jenkins/dev-aws/aws-handler.sh
+++ Deployment/Translator/ci-cd/jenkins/dev-aws/aws-handler.sh
@@ -13,16 +13,7 @@ echo "AWS handler : $handler"
 if [ "$handler" = "stop-containers" ] 
 then
 
-    ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ec2-user@${AWS_NAT_INSTANCE_DNS} PRIVATE_KEY_PATH=$PRIVATE_KEY_PATH \
-        AWS_DB_INSTANCE_DNS=$AWS_DB_INSTANCE_DNS CONTAINER_NAME=$DB_CONTAINER_NAME awsPemKey=${awsPemKey}\
-        'sh -s' <<-'ENDSSH'
-
-        echo "$CONTAINER_NAME"
-        ssh -o StrictHostKeyChecking=no ubuntu@$AWS_DB_INSTANCE_DNS CONTAINER_NAME=$CONTAINER_NAME 'sh -s' <<-'ENDSSH2' 
-
-        echo "$CONTAINER_NAME"
-ENDSSH2
-ENDSSH
+    ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ec2-user@${AWS_NAT_INSTANCE_DNS} 'sh -s' < db2-instance.sh
 
     ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ubuntu@${AWS_API_INSTANCE_DNS} AWS_API_INSTANCE_DNS=${AWS_API_INSTANCE_DNS} CONTAINER_NAME=${API_CONTAINER_NAME} \
         'sh -s' < ${CICD_SCRIPT_LOCATION}/docker-handler.dev-aws.sh cleanup
@@ -30,18 +21,18 @@ ENDSSH
     ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ubuntu@${AWS_WEB_INSTANCE_DNS} AWS_API_INSTANCE_DNS=${AWS_WEB_INSTANCE_DNS} CONTAINER_NAME=${WEB_CONTAINER_NAME} \
         'sh -s' < ${CICD_SCRIPT_LOCATION}/docker-handler.dev-aws.sh cleanup
 
-    #scp -r -i ${awsPemKey} -o StrictHostKeyChecking=no ${CICD_SCRIPT_LOCATION}/aws-handler.sh ec2-user@${AWS_NAT_INSTANCE_DNS}:${AWS_NAT_WORKDIR}
 
 # 1st approach
-
+# Its keep giving an error "Permission denied (publickey)."
 #     ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ec2-user@${AWS_NAT_INSTANCE_DNS} PRIVATE_KEY_PATH=$PRIVATE_KEY_PATH \
-#         AWS_DB_INSTANCE_DNS=$AWS_DB_INSTANCE_DNS CONTAINER_NAME=$DB_CONTAINER_NAME awsPemKey=${awsPemKey}\
-#         'sh -s' <<-'ENDSSH'
+#             AWS_DB_INSTANCE_DNS=$AWS_DB_INSTANCE_DNS CONTAINER_NAME=$DB_CONTAINER_NAME awsPemKey=${awsPemKey}\
+#             'sh -s' <<-'ENDSSH'
 
-#         echo "$CONTAINER_NAME"
-#         ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ubuntu@${AWS_DB_INSTANCE_DNS} CONTAINER_NAME=$CONTAINER_NAME 'sh -s' < aws-handler.sh stop-containers 
-#         exit
+#             echo "$CONTAINER_NAME"
+#             ssh -o StrictHostKeyChecking=no ubuntu@$AWS_DB_INSTANCE_DNS CONTAINER_NAME=$CONTAINER_NAME 'sh -s' <<-'ENDSSH2' 
 
+#             echo "$CONTAINER_NAME"
+# ENDSSH2
 # ENDSSH
 
 # 2nd approach - 
@@ -70,9 +61,10 @@ then
     scp -r -i ${awsPemKey} -o StrictHostKeyChecking=no ${WORKSPACE}/translator.tar.gz ubuntu@${AWS_API_INSTANCE_DNS}:${AWS_DEFAULT_WORKDIR}
     scp -r -i ${awsPemKey} -o StrictHostKeyChecking=no ${WORKSPACE}/translator.tar.gz ubuntu@${AWS_WEB_INSTANCE_DNS}:${AWS_DEFAULT_WORKDIR}
 
-    # Copy script files to execute on DB instance manually
+    # Copy script files to execute on DB instance manually from NAT instance
     scp -r -i ${awsPemKey} -o StrictHostKeyChecking=no ${CICD_SCRIPT_LOCATION}/db2-instance.sh ec2-user@${AWS_NAT_INSTANCE_DNS}:${AWS_NAT_WORKDIR}
 
+    # Extract project artifacts on API and WEB instances
     ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ubuntu@${AWS_API_INSTANCE_DNS} \
         'sh -s' <<-'ENDNATSSH'
 
