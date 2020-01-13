82a1c30fdd9b256f645cb88596051afebf03109a
rishirajbansal <rishiraj.architect@gmail.com>
2020-01-13 09:07:06 +0530

    Updated for Single Instance

---
+++ Deployment/Translator/ci-cd/jenkins/dev-aws/single-instance/aws-handler.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# AWS environment Setup
+
+set -x #echo on
+
+handler=$1
+awsPemKey=$2
+
+echo "AWS handler : $handler"
+
+
+if [ "$handler" = "stop-containers" ] 
+then
+
+    # EC2 Instance
+    ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ubuntu@${AWS_EC2_INSTANCE_DNS} WEB_CONTAINER_NAME=${WEB_CONTAINER_NAME} \
+        API_CONTAINER_NAME=${API_CONTAINER_NAME} DB_CONTAINER_NAME=${DB_CONTAINER_NAME} \
+        DOCKER_COMPOSE_FILE=${DOCKER_COMPOSE_FILE} DOCKER_LOG_LEVEL=${DOCKER_LOG_LEVEL} \
+        'sh -s' < ${CICD_SCRIPT_LOCATION}/docker-handler.dev-aws.sh cleanup
+
+elif [ "$handler" = "copy-project" ]
+then
+   
+    cd ${WORKSPACE}
+    tar -czf translator.tar.gz translator
+
+    # Copy Project Artifacts to EC2 instance
+    scp -r -i ${awsPemKey} -o StrictHostKeyChecking=no ${WORKSPACE}/translator.tar.gz ubuntu@${AWS_EC2_INSTANCE_DNS}:${AWS_EC2_WORKDIR}
+
+    # Extract project artifacts
+    ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ubuntu@${AWS_EC2_INSTANCE_DNS} \
+        'sh -s' <<-'ENDNATSSH'
+
+        rm -rf translator
+        tar -xf translator.tar.gz
+        rm translator.tar.gz
+
+ENDNATSSH
+
+elif [ "$handler" = "build" ]
+then
+
+    # EC2 Instance
+    ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ubuntu@${AWS_EC2_INSTANCE_DNS} \
+        DOCKER_COMPOSE_FILE=${DOCKER_COMPOSE_FILE} DOCKER_PROJECT_NAME=${DOCKER_PROJECT_NAME} \
+        DB_SERVICE_NAME=${DB_SERVICE_NAME} API_SERVICE_NAME=${API_SERVICE_NAME} WEB_SERVICE_NAME=${WEB_SERVICE_NAME} \
+        'sh -s' < ${CICD_SCRIPT_LOCATION}/docker-handler.dev-aws.sh build
+
+elif [ "$handler" = "up" ]
+then
+
+    # EC2 Instance
+    ssh -i ${awsPemKey} -o StrictHostKeyChecking=no ubuntu@${AWS_EC2_INSTANCE_DNS} \
+        DOCKER_COMPOSE_FILE=${DOCKER_COMPOSE_FILE} DOCKER_LOG_LEVEL=${DOCKER_LOG_LEVEL} \
+        'sh -s' < ${CICD_SCRIPT_LOCATION}/docker-handler.dev-aws.sh up
+
+elif [ "$handler" = "postdeploy" ]
+then
+
+    echo "Access Web Application : http://$AWS_EC2_IP:$AWS_WEB_PORT"
+    echo "Access API Application : http://$AWS_EC2_IP:$AWS_API_PORT"
+
+else
+    echo "Invalid AWS Handler passed to script."
+fi
---
+++ Deployment/Translator/ci-cd/jenkins/dev-aws/single-instance/docker-handler.dev-aws.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Docker environment cleanup Script
+
+set -x #echo on
+
+handler=$1
+
+echo "Docker handler : $handler"
+
+if [ "$handler" = "cleanup" ] 
+then
+    # Stop the containers:
+    docker container stop ${WEB_CONTAINER_NAME}
+    echo "${WEB_CONTAINER_NAME} Stopped."
+    docker container stop ${API_CONTAINER_NAME}
+    echo "${API_CONTAINER_NAME} Stopped."
+    docker container stop ${DB_CONTAINER_NAME}
+    echo "${DB_CONTAINER_NAME} Stopped."
+    
+        
+    # Delete preexisted containers:
+    docker container rm --force ${DB_CONTAINER_NAME}
+    echo "${DB_CONTAINER_NAME} Removed."
+    docker container rm --force ${API_CONTAINER_NAME}
+    echo "${API_CONTAINER_NAME} Removed."
+    docker container rm --force ${WEB_CONTAINER_NAME}
+    echo "${WEB_CONTAINER_NAME} Removed."
+        
+    # Bring compose down:
+    cd translator/deployment/compose
+    docker-compose -f ${DOCKER_COMPOSE_FILE} --log-level ${DOCKER_LOG_LEVEL} down 
+
+    # Remove unused images
+    docker rmi $(docker images -f 'dangling=true' -q) || true
+    # Remove unused volumes
+    docker volume rm $(docker volume ls -q --filter "dangling=true") || true
+
+elif [ "$handler" = "build" ]
+then
+
+    cd translator/deployment/compose
+    docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${DOCKER_PROJECT_NAME} build ${DB_SERVICE_NAME}
+    docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${DOCKER_PROJECT_NAME} build ${API_SERVICE_NAME}
+    docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${DOCKER_PROJECT_NAME} build ${WEB_SERVICE_NAME}
+
+elif [ "$handler" = "up" ]
+then
+
+    cd translator/deployment/compose
+  	docker-compose -f ${DOCKER_COMPOSE_FILE} --log-level ${DOCKER_LOG_LEVEL} up -d
+
+else
+    echo "Invalid Docker Handler passed to script."
+fi
+
